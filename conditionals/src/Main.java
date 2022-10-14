6469f1e4a047a95771e2a5eb3561b55477e1a8ad
nazlu <77505072+nazlu@users.noreply.github.com>
2022-10-14 15:13:34 +0300

    Initial commit

---
+++ conditionals/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int sayi=28;
+        if (sayi<20){
+            System.out.println("sayı 20'den küçüktür");
+        }else if (sayi==20){
+            System.out.println("sayı 20'ye eşitir");
+        }else {
+            System.out.println("sayı 20'den büyüktür");
+        }
+
+
+    }
+}
---
+++ datatypes/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double sayi =12.5;
+        sayi=13;
+        String sehir ="Ankara";
+        char karakter ='A';
+        boolean dogruMu=false;
+    }
+}
